d29255536e8ccb26e0050b6dbd7cdeb4c51ced74
Cho Lai Lam <107162584+cholailam@users.noreply.github.com>
2024-09-28 09:37:30 +0800

    Update lab04ex6.c

--- lab04ex6.c
+++ lab04ex6.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(){
@@ -15,8 +16,8 @@ int main(){
         if (x_diff == 0 && y_diff == 0)
             break;
 
-       
         min += 1;
+        
         if (i != spend)
             scanf(" %d", & next);
 
@@ -41,28 +42,56 @@ int main(){
         }
 
         
+        
+        if (x_diff == 0 && y_diff == 0)
+            break;
+
+        
+        printf("%d  iron: %d, %d\n", i, x_diff, y_diff);
 
         // move ourselves
         
-        if ( (x_diff >= y_diff && x_diff != 0) && !(x_diff == 1 && next == 2) && !(x_diff == -1 && next == 1) ){
+        if ( abs(x_diff) == abs(y_diff) ){
+
+            //printf("zero: %d\n", next);
+            if (next == 1 || next == 2){
+                
+                if (y_diff > 0)
+                    y_diff -= 1;
+                else y_diff += 1;
             
+            } else{
+
+                if (x_diff > 0)
+                    x_diff -= 1;
+                else x_diff += 1;
+
+            }
+
+        } else if ( abs(x_diff) > abs(y_diff) ){
+            
+            //printf("first: %d\n", next);
             if (x_diff > 0)
                 x_diff -= 1;
             else if (x_diff < 0)
                 x_diff += 1;
             
-        } else if ( (y_diff != 0) && !(y_diff == 1 && next == 3) && !(y_diff == -1 && next == 4) ){
+        } else{
 
+            //printf("second: %d\n", next);
             if (y_diff > 0)
                 y_diff -= 1;
             else if (y_diff < 0)
                 y_diff += 1;
 
-        }
+        } 
 
+        printf("%d    me: %d, %d\n\n", i, x_diff, y_diff);
 
         move_iron = next;
 
+        if (x_diff == 0 && y_diff == 0)
+            break;
     }
     
     if (i-1 == spend && x_diff != 0 || y_diff != 0)
